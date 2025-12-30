c9abe9e927e1eba6fe9c17cce801952d8c82b943
vavlxxx <vladimir.nefedov357@gmail.com>
2025-12-30 22:53:13 +0500

    cli: added cli interface with argparse module

---
+++ src/caching_proxy/cli.py
@@ -0,0 +1,66 @@
+import argparse
+
+from caching_proxy.cache import cache
+from caching_proxy.config import settings
+
+
+def build_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="CLI for Caching Proxy Server",
+        epilog="Run `caching-proxy --help` for more information",
+    )
+    subparsers = parser.add_subparsers(
+        required=True,
+        dest="command",
+    )
+    parser_run = subparsers.add_parser(
+        "run",
+        help="Run the caching proxy server",
+    )
+    parser_clear = subparsers.add_parser(
+        "clear",
+        help="Clear the cache",
+    )
+    parser_run.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=settings.CACHE_DEFAULT_PORT,
+        help="Port on which the server will run. Default value is %s" % settings.CACHE_DEFAULT_PORT,
+    )
+    parser_run.add_argument(
+        "-o",
+        "--origin",
+        type=str,
+        required=True,
+        help="Origin server URL from which requests will be proxied",
+    )
+    parser_run.add_argument(
+        "--ttl",
+        type=int,
+        default=settings.CACHE_DEFAULT_TTL,
+        help="TTL for cache entries. Default value is %s" % settings.CACHE_DEFAULT_TTL,
+    )
+    parser_run.set_defaults(func=run_proxy)
+    parser_clear.set_defaults(func=clear_cache)
+    return parser
+
+
+def run_proxy(args):
+    print("Running Caching Proxy Server... on port %s" % args.port)
+    print("Requests will be proxied from %s" % args.origin)
+
+
+def clear_cache(args):
+    cache.clear()
+    print("Cache is now empty...")
+
+
+def main():
+    parser = build_parser()
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
--- src/caching_proxy/config.py
+++ src/caching_proxy/config.py
@@ -5,6 +5,7 @@ class ProxyConfig(BaseSettings):
     CACHE_DEFAULT_TTL: int = 60
     CACHE_DEFAULT_KEY_EXPIRES_AT: str = "expires_at"
     CACHE_DEFAULT_KEY_VALUE: str = "value"
+    CACHE_DEFAULT_PORT: int = 3000
 
 
 settings = ProxyConfig()
